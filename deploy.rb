8223736471acf88a9ad357b136e7f03baacb83b9
Hayden Wei <haidongrun@gmail.com>
2015-12-09 10:33:46 +0800

    add deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-%r(git checkout gh-pages)
-%r(cp -rf ./_site/posts/* .)
-%r(rm -rf ./_site/posts)
-%r(git add -A)
-%r(git commit -m "Deploy: #{Time.now.to_s}")
-%r(git push origin gh-pages)
+%x(git checkout gh-pages)
+%x(cp -rf ./_site/posts/* .)
+%x(rm -rf ./_site/posts)
+%x(git add -A)
+%x(git commit -m "Deploy: #{Time.now.to_s}")
+%x(git push origin gh-pages)

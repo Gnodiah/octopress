1177b083e8b8f8bec31875c71dbe9b1da5a9f9fb
Hayden Wei <haidongrun@gmail.com>
2015-12-09 10:41:45 +0800

    update deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -3,6 +3,7 @@
 %x(git checkout gh-pages)
 %x(cp -rf ./_site/posts/* .)
 %x(rm -rf ./_site/posts)
+%x(rm -rf ./posts)
 %x(git add -A)
 %x(git commit -m "Deploy: #{Time.now.to_s}")
 %x(git push origin gh-pages)
